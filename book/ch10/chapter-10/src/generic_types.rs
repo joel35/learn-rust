6cc093f01de628063fda8f2f27c4b08980b8f99b
joel35 <system@icenilabs.com>
2022-04-27 16:47:59 +0100

    restructured ch10

---
+++ book/ch10/chapter-10/src/generic_types.rs
@@ -0,0 +1,47 @@
+pub struct Point<X1, Y1> {
+    pub x: X1,
+    pub y: Y1,
+}
+
+impl<X1, Y1> Point<X1, Y1> {
+    pub fn x(&self) -> &X1 {
+        &self.x
+    }
+
+    pub fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+impl Point<f32, f32> {
+    pub fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+
+use std::fmt::Display;
+
+pub struct Pair<T> {
+    pub x: T,
+    pub y: T,
+}
+
+impl<T> Pair<T> {
+    pub fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    pub fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self y);
+        }
+    }
+}
---
+++ book/ch10/chapter-10/src/main.rs
@@ -0,0 +1,80 @@
+mod remove_duplication;
+mod generic_types;
+
+use generic_types::{Point, Pair};
+use aggregator::{self, NewsArticle, Summary, Tweet};
+
+fn main() {
+    play_with_remove_dupe();
+    println!();
+    play_with_generic_types();
+    println!();
+    play_with_traits();
+}
+
+fn play_with_remove_dupe() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = remove_duplication::largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    let result = remove_duplication::largest(&number_list);
+    println!("The largest number is {}", result);
+}
+
+fn play_with_generic_types() {
+    let f = Point { x: 5.0, y: 11.1 };
+    dbg!(f.distance_from_origin());
+
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
+    let p3 = p1.mixup(p2);
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+}
+
+fn play_with_traits() {
+    let tweet = Tweet {
+        username: "horse_ebooks".to_string(),
+        content: "of course, as you probably already know, people".to_string(),
+        reply: false,
+        retweet: false,
+    };
+    println!("1 new tweet: {}", tweet.summarize());
+
+
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from(
+            "The Pittsburgh Penguins once again are the best \
+             hockey team in the NHL.",
+        ),
+    };
+
+    println!("New article available! {}", article.summarize());
+
+    aggregator::notify(&tweet);
+    aggregator::notify(&article);
+
+    let tweet = returns_summarizable();
+}
+
+fn returns_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    }
+}
+
+fn play_with_conditionals() {
+    let p =
+
+    p.cmp_display()
+}
